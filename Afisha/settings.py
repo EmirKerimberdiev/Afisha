1525c42398cb01ca3bd860690ead07db31825c94
EmirKerimberdiev <keremberdievemir43@gmail.com>
2024-12-20 05:59:24 +0600

    add registration authorization and confirm

--- Afisha/settings.py
+++ Afisha/settings.py
@@ -27,7 +27,16 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     "movie_app",
     'rest_framework',
+    'users',
+    'rest_framework.authtoken',
+
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+       'rest_framework.authentication.TokenAuthentication',
+    ],
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
---
+++ users/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.contrib.auth.models import User
+import random
+
+class ConfirmationCode(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='confirmation_code')
+    code = models.CharField(max_length=6)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    @staticmethod
+    def generate_code():
+        return str(random.randint(100000, 999999))
+
+    print(generate_code())
+
